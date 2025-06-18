00ce068bc0c45b89debcc47e551b078bf66a5751
Armirene-Iva-Devia <110568124+ideviacosta@users.noreply.github.com>
2025-06-18 01:04:20 -0500

    Update IRestaurantePersistencePort.java

--- src/main/java/com/pragma/powerup/plazoleta/domain/spi/IRestaurantePersistencePort.java
+++ src/main/java/com/pragma/powerup/plazoleta/domain/spi/IRestaurantePersistencePort.java
@@ -2,7 +2,6 @@ package com.pragma.powerup.plazoleta.domain.spi;
 
 import com.pragma.powerup.plazoleta.domain.model.PaginaRespuesta;
 import com.pragma.powerup.plazoleta.domain.model.Restaurante;
-import org.springframework.data.domain.Page;
 
 public interface IRestaurantePersistencePort {
     void guardarRestaurante(Restaurante restaurante);
